c3d36f8c7a91a3a4568ea9bce68e9fcf5ffea57a
Justyna Kuchta <justyna.kuchta@gmail.com>
2019-07-20 21:25:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,12 +5,8 @@ def load_library(file_path)
   library = YAML.load_file(file_path)
   hash = {"get_meaning" => {}, "get_emoticon" => {}}
   library.each do |meaning, array|
-    if !hash["get_meaning"].has_key?(array[1])
       hash["get_meaning"][array[1]] = meaning
-    end
-    if !hash["get_emoticon"].has_key?(array[0])
       hash["get_emoticon"][array[0]] = array[1]
-    end
   end
   hash
 end
