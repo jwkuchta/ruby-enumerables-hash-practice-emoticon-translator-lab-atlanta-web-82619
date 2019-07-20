7bf6a34a212d47aa1b4921cd465221a336d5775b
Justyna Kuchta <justyna.kuchta@gmail.com>
2019-07-20 22:04:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
   if library["get_emoticon"].has_key?(emoticon)
-    return library["get_emoticon"](emoticon)
+    return library["get_emoticon"][emoticon]
   else
     return "Sorry, that emoticon was not found"
   end
