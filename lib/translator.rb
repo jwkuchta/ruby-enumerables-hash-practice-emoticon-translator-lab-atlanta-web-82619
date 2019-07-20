f5a732580025de191be57be629ff0194c88499b0
Justyna Kuchta <justyna.kuchta@gmail.com>
2019-07-20 21:45:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,8 @@ def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
   result = nil
   library.each do |meaning, array|
-    if emoticon == array[0]
-      result = array[1]
+    if emoticon == array[1]
+      result = array[0]
     end
     result
   end
