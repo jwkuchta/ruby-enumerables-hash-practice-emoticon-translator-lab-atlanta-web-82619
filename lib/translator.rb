6484d4c6e60e29d00ee371bbb0cd1252af8ffbac
Justyna Kuchta <justyna.kuchta@gmail.com>
2019-07-20 20:58:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,29 @@
 require "yaml"
 emoticons = YAML.load_file('./lib/emoticons.yml')
 
-puts emoticons
+def load_library(file_path)
+  library = YAML.load_file(file_path)
+  hash = {}
 
-
-
-def load_library
+  library.each do |description, array|
+    array.each do |e_emoticon, j_emotiocon|
+      if !hash.has_key?("get_meaning")
+        hash["get_meaning"] = {}
+      end
+      if !hash["get_meaning"].has_key?(e_emoticon)
+        hash["get_meaning"]["#{e_emoticon}"] = description[0]
+      end
+      if !hash.has_key?("get_emoticon")
+        hash["get_emoticon"] = {}
+      end
+      if !hash["get_emoticon"].has_key?(j_emoticon)
+        hash["get_emoticon"]["#{j_emoticon}"] = description[1]
+      end
+    end
+  end
+  hash 
+end
+      
 
   # code goes here
 end
